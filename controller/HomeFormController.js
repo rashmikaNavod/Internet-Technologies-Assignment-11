963297f849633257fe35406b91d11b227638589f
rashmikaNavod <drashmikanavod@gmail.com>
2024-11-02 01:09:45 +0530

    issues fix

---
+++ controller/HomeFormController.js
@@ -0,0 +1,11 @@
+import { customer_array,item_array,order_array } from "../db/Database.js";
+
+
+$("#home").on('click',function(){
+    let customer_count = customer_array.length;
+    $("#customer-count").text(customer_count);
+
+    let item_count = item_array.length;
+    $("#item-count").text(item_count);
+});
+
